517c22ffd351ffdfd65f2cd68017ed8eef6656a4
Jordan Mavrogeorge <jordomav@gmail.com>
2018-07-24 16:45:28 -0700

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HomeComponent} from "./home/home.component";
+import {ChatComponent} from "./chat/chat.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {LoginComponent} from "./login/login.component";
+import {RegsiterComponent} from "./regsiter/regsiter.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'chat', component: ChatComponent},
+  {path: 'profile/:id', component: ProfileComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegsiterComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
